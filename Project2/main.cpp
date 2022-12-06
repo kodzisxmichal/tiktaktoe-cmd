735bb6c4928b3925e1af8c360aafff3d53d9ae95
s27121 - 209 <kodzissmichal@gmail.com>
2022-12-06 16:12:22 +0100

    ttt v2 - zepsuta granica

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,18 +1,63 @@
 #include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
+#include <windows.h>
 #include <cstdlib>
 
 using namespace std;
 
 void CreateGrid();
+void gotoxy(int x,int y);
+int cursorLimit(int cordinates);
+
 int main(){
 
     bool exit;
-    
+    char input;
+
+    int x=0;
+    int y=0;
+
     while(!exit){
         CreateGrid();
 
+        //naprawic granice (musza sie wykonywac przed przesunieciem)
+        if(x>5) x=5;
+        if(y>5) y=5;
+        if(x<0) x=0;
+        if(y<0) y=0;
+        gotoxy(x,y);
+
+        input = getch();
+        switch (input)
+        {
+            case 97:
+                x-=2;
+                break;
+            
+            case 100:
+                x+=2;
+                break;
+
+            case 119:
+                y-=2;
+                break;
+
+            case 115:
+                y+=2;
+                break;
+
+            case 113:
+                exit=true;
+                break;
+
+            default:
+                break;
+        }
+
         system("CLS");
-    }
+        }
 
 
     return 0;
@@ -38,4 +83,24 @@ void CreateGrid(){
         cout << endl;
     }
 
+}
+
+void gotoxy(int x,int y)    
+{
+    COORD c;
+    c.X = x - 1;
+    c.Y = y - 1;
+    SetConsoleCursorPosition( GetStdHandle( STD_OUTPUT_HANDLE ), c );
+}
+
+int cursorLimit(int cordinates){
+    if(cordinates<0){
+        return 0;
+    }
+    else if(cordinates>5){
+         return 5;
+    }
+    else{
+        return cordinates;
+    }
 }
